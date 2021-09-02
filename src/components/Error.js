24c60cf37af9c54542abd4c33d223f383cda96eb
Esra Cebi <esracebi.81@gmail.com>
2021-09-02 18:06:08 +0300

    çekilen karakterlerin listelenmesi

---
+++ src/components/Error.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Error({message}) {
+    return (
+        <div>
+            Error :{message}
+            
+        </div>
+    )
+}
+
+export default Error
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,21 +1,50 @@
-import {useEffect} from 'react';
+import { useEffect } from 'react';
 
-import { useSelector, useDispatch} from "react-redux";
-import   {fetchCharacters }  from "../../redux/charactersSlice"
+import Masonry from 'react-masonry-css';
+import "./styles.css";
 
-function Home () {
-    const data = useSelector((state) => state.characters);
+import Loading from '../../components/Loading';
+import Error from "../../components/Error"
+
+import { useSelector, useDispatch } from "react-redux";
+import { fetchCharacters } from "../../redux/charactersSlice";
+
+
+function Home() {
+    const characters = useSelector((state) => state.characters.items);
+    const isLoading = useSelector((state) => state.characters.isLoading);
+    const error = useSelector((state) => state.characters.error);
     const dispatch = useDispatch();
 
     useEffect(() => {
         dispatch(fetchCharacters());
     }, [dispatch]);
-    console.log(data);
-    
+
+    if(isLoading){
+        return <Loading />;
+    }
+    if(error){
+        return <Error message={error} />
+    }
+
+
     return (
         <div>
-            home
-            
+            <Masonry
+                breakpointCols={4}
+                className="my-masonry-grid"
+                columnClassName="my-masonry-grid_column">
+                {
+                characters.map(character => (
+                    <div key={character.char_id}>
+                        <img alt={character.name} src={character.img} className="character" />
+                        <div className="char_name">{character.name}</div>
+                    </div>
+                ))
+                }
+                
+            </Masonry>
+
         </div>
     )
 }
