9d58ec1d01a86216c3f5172f110e5da9b0912351
Esra Cebi <esracebi.81@gmail.com>
2021-09-02 17:33:40 +0300

    karakterlerin apiden çekilmesi

--- src/App.js
+++ src/App.js
@@ -4,54 +4,23 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
 } from "react-router-dom";
+
+//pages
+import Home from './pages/Home';
+
 
 function App() {
   return (
-    <div className="App">
-          <Router>
+    <Router>
       <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/users">Users</Link>
-            </li>
-          </ul>
-        </nav>
         <Switch>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/users">
-            <Users />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
+          <Route path="/" component={Home} />
         </Switch>
       </div>
     </Router>
-
-    </div>
   );
 }
-function Home() {
-  return <h2>Home</h2>;
-}
 
-function About() {
-  return <h2>About</h2>;
-}
-
-function Users() {
-  return <h2>Users</h2>;
-}
 
 export default App;
---
+++ src/pages/Home/index.js
@@ -0,0 +1,23 @@
+import {useEffect} from 'react';
+
+import { useSelector, useDispatch} from "react-redux";
+import   {fetchCharacters }  from "../../redux/charactersSlice"
+
+function Home () {
+    const data = useSelector((state) => state.characters);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(fetchCharacters());
+    }, [dispatch]);
+    console.log(data);
+    
+    return (
+        <div>
+            home
+            
+        </div>
+    )
+}
+
+export default Home;
