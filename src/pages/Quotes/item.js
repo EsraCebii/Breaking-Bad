1ff896f6fe3c18ed02454c4d7fcbbd27cf04a155
Esra Cebi <esracebi.81@gmail.com>
2021-09-03 13:09:37 +0300

    quotes route hazırlanması

---
+++ src/pages/Quotes/item.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./style.css";
+
+function Item({item}) {
+    return (
+        <div className="quote_item">
+            <q>{item.quote}</q>
+            <strong>{item.author}</strong>
+        </div>
+    )
+}
+
+export default Item
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
-import  charactersSlice from "./charactersSlice"
+
+import  charactersSlice from "./charactersSlice";
+import  quotesSlice from "./guotesSlice"
 
 export  const store = configureStore({
     reducer: {
         characters: charactersSlice,
+        quotes: quotesSlice,
     },
 });
